44e8794353b48670a3a4af3d3c3637ad4975600c
Dylan Shaffer <13596307+WRX-Dylan@users.noreply.github.com>
2022-09-12 11:08:12 -0400

    Add project files.

---
+++ AttributeRouting/Controllers/Demo1Controller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AttributeRouting.Controllers;
+
+[Route("lec/demoone")]
+public class Demo1Controller : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
